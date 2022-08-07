ddbf70717ece9d852b7a4437f155cc625c50b5fd
vanluijt <vl54@ziggo.nl>
2022-08-07 11:23:50 +0200

    Initial Commit

---
+++ rechthoeken/rechthoekenApp.swift
@@ -0,0 +1,17 @@
+//
+//  rechthoekenApp.swift
+//  rechthoeken
+//
+//  Created by P.A. van Luijt on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct rechthoekenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
